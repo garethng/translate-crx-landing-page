22edbd4246d208acb7acd87cbd32897f5f97b9a4
Gareth Ng <njwqz77@gmail.com>
2024-08-06 21:35:46 +0800

    support edge and chrome

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -1,33 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { Image } from "@nextui-org/react";
 import Link from "next/link";
 
 const CTAButton = ({ locale,btntype }: { locale: any, btntype: string  }) => {
   let svgCode = "";
   let className = "";
+  let store = ""
+  let link = ""
   if (btntype === "chrome") {
-    svgCode = 'https://fonts.gstatic.com/s/i/productlogos/chrome_store/v7/192px.svg';
+    svgCode = '/images/chromestore.svg';
     className = "flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white h-12"
+    store = locale.storeChrome
+    link = "https://chromewebstore.google.com/detail/auto-scroll/cmbpfcapjacpbmehmemljdilabjnlfhb"
   } else if (btntype === "edge") {
-    svgCode = 'https://edgestatic.azureedge.net/shared/cms/lrs1c69a1j/section-images/29bfeef37eef4ca3bcf962274c1c7766.png'
-    className = "flex items-center gap-2 bg-white-500 hover:bg-white-600 text-balck h-12 border-sky-500 border-solid"
+    svgCode = '/images/edge.png'
+    className = "flex items-center gap-2 bg-white-500 hover:bg-white-600 text-balck h-12 border border-gray-500"
+    store = locale.storeEdge
+    link = "https://microsoftedge.microsoft.com/addons/detail/auto-scroll/lnmdcgidhppfifogfeejdbbabheijcjl"
   }
 
   return (
     <Link
-      href="https://chromewebstore.google.com/detail/auto-scroll/cmbpfcapjacpbmehmemljdilabjnlfhb"
+      href={link}
       target="_blank"
       rel="noopener noreferrer nofollow"
     >
       <Button
         variant="default"
         className={ className }
-        aria-label="Get Boilerplate"
+        aria-label="Get App"
       >
-        <img src={svgCode} className="h-8"/>
+        <Image
+          src={svgCode}
+          className="h-8"
+          alt="store"
+        />
 
         <div className="flex flex-col top-4">
           <p className="text-xs text-start">{locale.title}</p>
-          <p className="text-lg text-center">{locale.store}</p>
+          <p className="text-lg text-center">{store}</p>
         </div>
         
       </Button>
